03f13aeb141417ee7c8802fd82740a4306810647
Jhonathan Velez <kokoa.arte@gmail.com>
2024-09-12 13:46:02 -0500

    iniitla commit

--- src/Config/Config.jsx
+++ src/Config/Config.jsx
@@ -1,16 +1,16 @@
 export const config = {
-   //serverRoute: "https://test-prueba-production.up.railway.app"
-   serverRoute: "http://localhost:3001"
+   serverRoute: "https://test-prueba-production.up.railway.app"
+   //serverRoute: "http://localhost:3001"
  }
  
  export const SocketRoute = {
-   // serverRoute: "https://railway.grupo-hoteles.com"
-    serverRoute: "http://localhost:3001"
+    serverRoute: "https://railway.grupo-hoteles.com"
+    //serverRoute: "http://localhost:3001"
  }
  
  export const CLoubedsRoute ={
-    //serverRoute: "https://railway.grupo-hoteles.com"
-    serverRoute:"http://localhost:3001"
+    serverRoute: "https://railway.grupo-hoteles.com"
+    //serverRoute:"http://localhost:3001"
  }
 
  export const Environment ={
